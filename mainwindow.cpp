3dcfd6fd454a272e7da381ca4d34d7d02d8ca668
Fabrice Ndui <fnduister@gmail.com>
2018-11-29 14:33:23 -0500

    ajout du design probleme avec les librairies qt

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actioncharger_lexique_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this,"Open the lexique");
+    QFile file(filename);
+    currentFile = filename;
+    if(!file.open(QIODevice::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this, "Warning", "Connot open file : "+ file.errorString());
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QFile>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,12 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actioncharger_lexique_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString currentFile = "";
 };
 
 #endif // MAINWINDOW_H
